00440940b8b1172874e4916349ea9e35694b1e30
2yunseong <dbsdltjd3701@naver.com>
2023-07-29 00:09:31 +0900

    feat: add test component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Test from './components/Test';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders test components', () => {
+  render(<Test />);
+  const testElement = screen.getByText(/test/i);
+  expect(testElement).toBeInTheDocument();
 });
